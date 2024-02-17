e3d9f0be16705feac06ed1bd6ffe4aaa6024c349
anjesh.ghimire <anjesh67890@gmail.com>
2024-02-17 22:15:51 +0545

    unnecessary footer removed bug fixed

--- src/components/dashboard/Sider.jsx
+++ src/components/dashboard/Sider.jsx
@@ -121,13 +121,6 @@ const DashBoardSider = () => {
         >
           <Outlet />
         </Content>
-        <Footer
-          style={{
-            textAlign: "center",
-          }}
-        >
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
